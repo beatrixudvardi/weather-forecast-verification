ca166dec8c917c2674369c1c9790b342a1bf5ba5
beatrixudvardi <udvbeatrix@gmail.com>
2026-09-01 17:55:15 +0000

    Improve weather data analysis

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
 
 url= "https://api.open-meteo.com/v1/forecast"
 
@@ -26,3 +27,14 @@ df['date'] = df['time'].dt.date
 #print(df.head())
 daily_max = df.groupby('date')['temperature'].max()
 print(daily_max)
+
+plt.plot(daily_max.index, daily_max.values, marker='o', linestyle='-', color='b'    )
+#plt.show()
+plt.xlabel("Date")
+plt.ylabel("Temperature (°C)")
+plt.title("Daily Maximum Temperature")
+plt.gca().xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%m/%d')) 
+plt.xticks(rotation=90)
+plt.savefig("daily_temperature.png")
+plt.show()
+
